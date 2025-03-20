44fd09b82dc7028330dd608645f002d140b330e2
ashx3s <ashlyn@lilyx.io>
2025-03-20 12:48:13 -0600

    site components starter content + style

---
+++ src/app/components/AppFooter.jsx
@@ -0,0 +1,7 @@
+export default function AppFooter() {
+  return (
+    <footer className='mx-4 lg:mx-12 my-8 grid place-items-center'>
+      <p>&copy; 2025 Ashlyn Knox</p>
+    </footer>
+  );
+}
---
+++ src/app/components/AppHeader.jsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+export default function AppHeader() {
+  return (
+    <header className='px-4 lg:px-12 py-8 flex justify-between items-center'>
+      <nav className='flex justify-between gap-4 grow-2'>
+        <div>
+          <Link href='/' className='block'>
+            LOGO
+          </Link>
+        </div>
+        <ul className='flex gap-4 bg-blue-500 mx-8'>
+          <li>
+            {/* Make this a protected route */}
+            <Link href='/todo-app'>TODO App</Link>
+          </li>
+          <li>
+            <Link href='/login'>Sign In</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className='flex gap-4 '>
+        {/* hrefDO: Logout buthrefn only visible when user is logged in */}
+        <button>Logout</button>
+        <div>AVATAR</div>
+      </div>
+    </header>
+  );
+}
