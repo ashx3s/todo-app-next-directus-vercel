1d2cd4fa7b8e37939e602eccb5b06015695da953
ashx3s <ashlyn@lilyx.io>
2025-03-20 13:02:08 -0600

    workflow note added

--- src/app/todo-app/page.jsx
+++ src/app/todo-app/page.jsx
@@ -3,7 +3,9 @@ export default function TODOApp() {
     <>
       <header>
         <h1>Hello USERNAME!</h1>
-        <p>What shall we plan today?</p>
+        <p>
+          Move the UI for the todo app here after setting it up in the home page
+        </p>
       </header>
       <section>{/* TODO: ADD todo interface here */}</section>
     </>
