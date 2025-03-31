ebd9fdbfbd687e2736ded3188bf7e2df2ebb5a00
ashx3s <ashlyn@lilyx.io>
2025-03-31 17:29:32 -0600

    todo server page

--- src/app/server-todo-app/page.jsx
+++ src/app/server-todo-app/page.jsx
@@ -1,30 +1,36 @@
 import directus from "@/lib/directus";
 import { readItem } from "@directus/sdk";
 import TodoListSWR from "../components/TodoListSWR";
-// On this page, we want to fetch data server side that doesn't change much
+import DisplaySanitizedSection from "../components/DisplaySanitizedSection";
+import DOMPurify from "isomorphic-dompurify";
 
-// Fetch Page Data function
+export const revalidate = 3600;
 
 export default async function Page() {
-  // data
+  const pageData = await directus.request(readItem("pages", "todoPage"));
+  const sanitizedContent = await DOMPurify.sanitize(pageData.content);
+  const data = { ...pageData, content: sanitizedContent };
   return (
     <>
       <header>
         <h1 className="text-2xl font-semibold">Server Side Page</h1>
-        <p>
+        <p className="max-w-prose">
           In this example, we are loading the page SSR and then the dynamic
           stuff is loaded CSR.{" "}
           <strong>this header is static content written in JSX</strong>
         </p>
       </header>
-      <section>
-        <h2>
-          The component below this heading is CSR to prioritize client
-          interactions
-        </h2>
+      <section className="bg-pink-900 p-4 rounded-md my-8">
+        <header>
+          <h2 className="text-2xl font-semibold">SWR Component</h2>
+          <p>
+            The following list is a component that uses SWR to make a client
+            side fetch to directus. However this text is static.
+          </p>
+        </header>
         <TodoListSWR />
       </section>
-      <section>{/* Data from directus */}</section>
+      <DisplaySanitizedSection sectionData={data} />
     </>
   );
 }
